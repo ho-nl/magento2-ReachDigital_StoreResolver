ca5b04ca250d1c2277ae73fee45a4e8b97e211b4
Jeroen van Leusden <jeroen@reachdigital.nl>
2018-02-27 11:59:43 +0100

    [PARA-511] Support Base URL with store code

--- README.md
+++ README.md
@@ -6,18 +6,28 @@ You are running a multi store with a specific catalog per domain.
 
 ## Installation
 ```BASH
-
 composer config repositories.honl/magento2-storeresolver vcs git@github.com:ho-nl/magento2-Ho_StoreResolver.git
 composer require honl/magento2-storeresolver
 ```
 
 ## Automatic mapping from Domain > Store View
-
 With the new implementation for the `StoreResolverInterface` the domains automatically get mapped to the correct store view. This reduces the need to modify code to get new domain names working.
 
 1. Register your domain
 2. Point the A-records to your server 
 3. ~~Change index.php, .htaccess or nginx_config file to activate your domain name~~ `StoreResolver` solves this for you.
+
+## Add Store Code to Base URL
+Store code will be removed from URL if added in the Base URL.
+Make sure to explicitly set `Base URL for Static View Files` and `Base URL for User Media Files` without the store code
+
+| Configuration                   | Value                       |
+| ------------------------------- | --------------------------- |
+| Base URL                        | https://paracord.eu/de/     |
+| Base URL for Static View Files  | https://paracord.eu/static/ |
+| Base URL for User Media Files   | https://paracord.eu/media/  |
+
+> It's not possible to have CMS pages with the same identifier as a store code!
 
 ## Store scope definition
 
---
+++ src/Model/Plugin/ModelBowerContextFactoryPlugin.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Ho\StoreResolver\Model\Plugin;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Asset\File\ContextFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use ReachDigital\Polymer\Model\BowerContextFactory;
+
+class ModelBowerContextFactoryPlugin
+{
+    /** @var \Magento\Framework\View\Asset\File\Context */
+    private $fileContext;
+
+    /** @var ContextFactory $contextFactory */
+    private $contextFactory;
+
+    /** @var UrlInterface $url */
+    private $url;
+
+    /** @var StoreManagerInterface $storeManager */
+    private $storeManager;
+
+    /**
+     * @param ContextFactory        $contextFactory
+     * @param UrlInterface          $url
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        ContextFactory $contextFactory,
+        UrlInterface $url,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->contextFactory = $contextFactory;
+        $this->url = $url;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @param BowerContextFactory $subject
+     * @param callable            $proceed
+     * @param string              $baseDirType
+     * @param string              $urlType
+     * @param string              $dirPath
+     *
+     * @return \Magento\Framework\View\Asset\File\Context
+     */
+    public function aroundGetFileContext(
+        BowerContextFactory $subject,
+        callable $proceed,
+        string $baseDirType,
+        string $urlType,
+        string $dirPath
+    ): \Magento\Framework\View\Asset\File\Context {
+        if ($this->fileContext === null) {
+            $this->fileContext = $this->contextFactory->create([
+                'baseUrl'       => $this->getProcessedUrl($urlType),
+                'baseDirType'   => $baseDirType,
+                'contextPath'   => $dirPath
+            ]);
+        }
+
+        return $this->fileContext;
+    }
+
+    /**
+     * Strip Store Code from url when added to Base url.
+     *
+     * @param string $urlType
+     *
+     * @return string
+     */
+    private function getProcessedUrl(string $urlType): string
+    {
+        $url = $this->url->getBaseUrl(['_type' => $urlType]);
+
+        $pos = strrpos(rtrim($url, '/'), '/');
+        if ($pos !== false) {
+            $storeCode = trim(substr($url, $pos), '/');
+
+            try {
+                $this->storeManager->getStore($storeCode);
+            } catch (NoSuchEntityException $e) {
+                return $url;
+            }
+
+            $url = substr($url, 0, $pos+1);
+        }
+
+        return $url;
+    }
+}
